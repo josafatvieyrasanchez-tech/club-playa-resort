55dca6f685b97c031c648f44b0eaf40e8dccc981
josafatvieyrasanchez-tech <josafatvieyrasanchez@gmail.com>
2026-06-04 16:41:49 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.post('/api/admin/estado-espacio', async (req, res) => {
 // Esto debe ir al final, después de tus rutas de API
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
